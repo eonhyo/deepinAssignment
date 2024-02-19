2a091cd7afef0974ce53efd8b79abed535a49fea
eonhyo <syuku94069@gmail.com>
2024-02-19 13:10:56 +0900

    라우터설정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,7 @@
 import Detail from "pages/Detail";
 import Home from "pages/Home";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
@@ -9,6 +11,8 @@ export default function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="*" element={<Navigate replace to="/" />} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
